91bae66c44124150397a5ef72939dfc188b635ef
EGOr-ProGrammar <need.extra.post@gmail.com>
2025-12-05 19:18:14 +0300

    feat(server): implement configuration loading in ServerMain

--- src/main/java/server/ServerMain.java
+++ src/main/java/server/ServerMain.java
@@ -3,10 +3,27 @@ package server;
 import server.controller.GameWorld;
 
 public class ServerMain {
-    public static void main(String[] args) throws Exception {
-        GameWorld world = new GameWorld();
-        TcpServer server = new TcpServer(4000, world);
-        // TODO: блокируется, ждёт клиента и крутит game-loop, а надо в real time
-        server.start();
+
+    public static void main(String[] args) {
+        try {
+            // Вся логика загрузки внутри ServerConfig
+            ServerConfig config = ServerConfig.load();
+
+            // Вывод и валидация
+            config.printConfiguration();
+            config.validate();
+
+            // Создание и запуск
+            GameWorld world = new GameWorld(config.getSeed());
+            TcpServer server = new TcpServer(config, world);
+
+            System.out.println("\nServer starting...\n");
+            server.start();
+
+        } catch (Exception e) {
+            System.err.println("Fatal error: " + e.getMessage());
+            e.printStackTrace();
+            System.exit(1);
+        }
     }
 }
