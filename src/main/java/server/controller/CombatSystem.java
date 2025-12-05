0daae503dd6142499372f91e81c7211b625c6283
EGOr-ProGrammar <need.extra.post@gmail.com>
2025-12-05 21:49:38 +0300

    feat(controller): new controller reliable for attacking mobs

---
+++ src/main/java/server/controller/CombatSystem.java
@@ -0,0 +1,68 @@
+package server.controller;
+
+import server.model.EnemyNpc;
+import server.model.LootPile;
+import server.model.Player;
+import shared.Position;
+
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
+/**
+ * Система боя и взаимодействий игрока с миром
+ */
+public class CombatSystem {
+
+    /**
+     * Атакует врага на целевой позиции, если он там есть
+     * @return true если атака произошла, false если нет цели
+     */
+    public static boolean tryAttackEnemy(Player player, Position targetPos,
+                                         Map<UUID, EnemyNpc> enemies) {
+        for (EnemyNpc enemy : enemies.values()) {
+            if (enemy.isAlive() &&
+                    enemy.getCurrentLevel() == player.getCurrentLevel() &&
+                    enemy.getMapX() == player.getMapX() &&
+                    enemy.getMapY() == player.getMapY() &&
+                    enemy.getPosition().equals(targetPos)) {
+
+                enemy.takeDamage(player.getAttack());
+                System.out.println("💥 Player hit enemy for " + player.getAttack() + " dmg");
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void pickupLootAtPosition(Player player, List<LootPile> lootPiles) {
+        Iterator<LootPile> it = lootPiles.iterator();
+        while (it.hasNext()) {
+            LootPile loot = it.next();
+            if (player.getCurrentLevel() == loot.getLevel() &&
+                    player.getMapX() == loot.getMapX() &&
+                    player.getMapY() == loot.getMapY() &&
+                    player.getPosition().equals(loot.getPosition())) {
+
+                player.addGold(loot.getGoldAmount());
+                System.out.println("💰 Player picked up " + loot.getGoldAmount() + " gold");
+                it.remove();
+            }
+        }
+    }
+
+    public static boolean isPlayerAt(Position targetPos, Player currentPlayer,
+                                     Map<String, Player> allPlayers) {
+        for (Player other : allPlayers.values()) {
+            if (other != currentPlayer &&
+                    other.getCurrentLevel() == currentPlayer.getCurrentLevel() &&
+                    other.getMapX() == currentPlayer.getMapX() &&
+                    other.getMapY() == currentPlayer.getMapY() &&
+                    other.getPosition().equals(targetPos)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
